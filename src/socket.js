730d7e129636193c15ed6a601d48c79bd942fab1
viniciussis <vinicius01012@gmail.com>
2024-09-23 10:00:03 -0300

    /refactor: changing from nodejs + js to nestjs + ts

--- src/socket.js
+++
@@ -1,9 +0,0 @@
-import io from "./server.js"
-
-io.on('connection', (socket) => {
-  console.log('Web Socket Connected!')
-
-  socket.on('typing_document', (text) => {
-    socket.broadcast.emit('sharing_document_data', text)
-  })
-})
